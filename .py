a605ea9c165f17a1680faaeb282f20eeb1cb5248
mceachec7630-design <mceachec7630@student.faytechcc.edu>
2026-02-09 16:28:40 -0500

    Add files via upload

---
+++ .py
@@ -0,0 +1,13 @@
+Chris McEachern
+02/09/26
+P1lab1
+input lab
+
+# Ask the user for their first name
+first_name = input("Please enter your first name: ")
+
+# Ask the user for their last name
+last_name = input("Please enter your last name: ")
+
+# Display the welcome message
+print("Welcome to the course,", first_name, last_name + "!")
