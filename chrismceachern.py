cc30a2af6eaf26c81d9e5451f36e0703fd2a19c2
mceachec7630-design <mceachec7630@student.faytechcc.edu>
2026-02-09 19:32:28 -0500

    Add files via upload

---
+++ chrismceachern.py
@@ -0,0 +1,8 @@
+
+
+
+
+name = input("Enter your first name: ") # notice collected information is assigned to a variable , variable names can be used to store information that can be used later in the program, in this case we are storing the user's name in a variable called name
+name = input("Enter your last name: ") # we are asking the user for their last name and storing it in the same variable name, this will overwrite the previous value stored in the variable name, so now name will only contain the user's last name
+print("Hello",print(f"Hello {first_name} {last_name}! Welcome to CTI-110.")
+ name) # print is a function that displays information to the user, it can display text and variables, in this case we are displaying a greeting followed by the user's name that we collected earlier
